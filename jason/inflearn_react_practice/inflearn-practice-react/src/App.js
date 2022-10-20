6d78bc0e2eed86815dbe6d6760c5b788949a09c5
KangukLee-97 <rkddnrdl97@naver.com>
2022-10-20 22:57:35 +0900

    Feat: Header, Navigation 구조 설정

---
+++ jason/inflearn_react_practice/inflearn-practice-react/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from "./Header/Header";
+import Navigation from "./Navigation/Nav";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
