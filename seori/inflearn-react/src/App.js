a9281c0ef70b9d128b35f12949e5b0570d523602
EunsuSeo01 <eunsu0613@naver.com>
2022-10-22 16:26:46 +0900

    Refactor: 파일 구조 변경

--- seori/inflearn-react/src/App.js
+++ seori/inflearn-react/src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
 import React from 'react';
-import Header from './Header/Header';
-import Main from './Main/Main';
-import Footer from './Footer/Footer';
+import MainPage from './Pages/MainPage';
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
+    <div>
+      <MainPage />
     </div>
   );
 }
---
+++ seori/inflearn-react/src/Pages/MainPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Components/Header";
+import Main from "../Components/Main";
+import Footer from "../Components/Footer";
+
+function MainPage() {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    );
+}
+
+export default MainPage;
