84014a32622f131e7d5699e29c28cf36dc8d159d
JM2308 <wlals2987@naver.com>
2022-11-05 01:36:46 +0900

    Feat: JavaScript Menu

--- luna/study_react/src/Week 07/MenuItem.js
+++ luna/study_react/src/Week 07/MenuItem.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function MenuItem() {
     return (
         <>
-            <img src="image\menuitem01.jpeg" alt="menuitem01"/>
+            <MenuImage src="image\menuitem01.jpeg" alt="menuitem01"/>
         </>
     )
 }
+
+const MenuImage = styled.img `
+    width: 24%;
+    height: 12%;
+    border-radius: 1em;
+    border: 4px solid #c8a46b;
+`
