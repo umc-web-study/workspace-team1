b62dd9239f25ebef0581e842503ba5ee434f0b07
JM2308 <wlals2987@naver.com>
2022-11-12 19:50:53 +0900

    Feat: React Tetris

--- luna/tetris-react/src/components/StartButton.js
+++ luna/tetris-react/src/components/StartButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import { StyledStartButton } from "./styles/StyledStartButton";
 
 const StartButton = ({ callback }) => (
-    <StyledStartButton onclick={callback}>Start Game</StyledStartButton>
+    <StyledStartButton onClick={callback}>Start Game</StyledStartButton>
 )
 
 export default StartButton;
--- luna/tetris-react/src/hooks/useStage.js
+++ luna/tetris-react/src/hooks/useStage.js
@@ -3,8 +3,22 @@ import { createStage } from "../gameHelpers";
 
 export const useStage = (player, resetPlayer) => {
     const [stage, setStage] = useState(createStage());
+    const [rowsCleared, setRowsCleared] = useState(0);
 
     useEffect(() => {
+        setRowsCleared(0);
+
+        const sweepRows = newStage =>
+            newStage.reduce((ack, row) => {
+                if (row.findIndex(cell => cell[0] === 0) === -1) {
+                    setRowsCleared(prev => prev + 1);
+                    ack.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+                    return ack;
+                }
+                ack.push(row);
+                return ack;
+            }, [])
+
         const updateStage = prevStage => {
             const newStage = prevStage.map(row =>
                 row.map(cell => (cell[1] === 'clear' ? [0, 'clear']: cell))
@@ -21,11 +35,16 @@ export const useStage = (player, resetPlayer) => {
                 });
             });
 
+            if (player.collided) {
+                resetPlayer();
+                return sweepRows(newStage);
+            }
+             
             return newStage;
         };
 
         setStage(prev => updateStage(prev));
-    }, [player]);
+    }, [player, resetPlayer]);
 
     return [stage, setStage];
 }
