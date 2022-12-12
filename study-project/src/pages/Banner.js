3609eaa29c88e7dc88d520686762aa4f692fc430
JM2308 <wlals2987@naver.com>
2022-12-12 19:28:07 +0900

    Feat: Main Banner

--- study-project/src/pages/Banner.js
+++ study-project/src/pages/Banner.js
@@ -26,49 +26,40 @@ const RightArrow = styled.div `
 `
 
 export default function Banner() {
-    const [number, setNumber] = useState(1);
-    const [bannerImage, setImageNumber] = useState(1);
+    const [bannerImageIdx, setImageNumber] = useState(1);
+
+    const bannerImage = [
+        "https://img2.joongna.com/media/original/2022/11/01/1667268265697.png",
+        "https://img2.joongna.com/media/original/2022/11/18/1668741211087.png",
+        "https://img2.joongna.com/media/original/2022/10/17/1665967916895.png",
+        "https://img2.joongna.com/media/original/2022/11/07/1667777988290.png",
+        "https://img2.joongna.com/media/original/2022/10/31/1667208266409.png",
+        "https://img2.joongna.com/media/original/2022/10/31/1667207459320.png",
+        "https://img2.joongna.com/media/original/2022/10/12/1665557839538.png",
+        "https://img2.joongna.com/media/original/2022/10/31/1667208784699.png",
+        "https://img2.joongna.com/media/original/2022/11/11/1668145504958.png",
+        "https://img2.joongna.com/media/original/2022/10/31/1667210920864.png"
+    ]
 
     const onIncreaseImage = () => {
-        setImageNumber(number + 1)
-
-        if (setImageNumber === 0)
-            bannerImage = "https://img2.joongna.com/media/original/2022/10/31/1667200204429.png"
-        else if (setImageNumber === 1)
-            bannerImage = `<img src="https://img2.joongna.com/media/original/2022/11/01/1667268265697.png" alt="banner"/>`
-        else if (setImageNumber === 2)
-            bannerImage = `<img src="https://img2.joongna.com/media/original/2022/11/01/1667268265697.png" alt="banner"/>`
-        else if (setImageNumber === 3)
-            bannerImage = `<img src="https://img2.joongna.com/media/original/2022/11/01/1667268265697.png" alt="banner"/>`
-        else if (setImageNumber === 4)
-            bannerImage = `<img src="https://img2.joongna.com/media/original/2022/11/01/1667268265697.png" alt="banner"/>`
-        else if (setImageNumber === 5)
-            bannerImage = `<img src="https://img2.joongna.com/media/original/2022/11/01/1667268265697.png" alt="banner"/>`
-        else if (setImageNumber === 6)
-            bannerImage = `<img src="https://img2.joongna.com/media/original/2022/11/01/1667268265697.png" alt="banner"/>`
+        if (bannerImageIdx < 9)
+            setImageNumber(bannerImageIdx + 1)
+        else setImageNumber(1);
     }
 
-    const onIncrease = () => {
-        if (number === 7)
-            setNumber(1)
-        else setNumber(number + 1);
-    }
-
-    const onDecrease = () => {
-        if (number === 1)
-            setNumber(7)
-        else setNumber(number - 1);
+    const onDecreaseImage = () => {
+        if (bannerImageIdx > 1)
+            setImageNumber(bannerImageIdx - 1)
+        else setImageNumber(9);
     }
 
     return (
         <BannerWrapper>
-            <LeftArrow>
+            <LeftArrow onClick={onDecreaseImage}>
                 {/* <img src="../images/left_arrow.png" alt="LeftArrow" /> */}
             </LeftArrow>
-            <BannerImg/>
-            {/* <img src={bannerImage} alt="BannerImage" /> */}
-            {/* {bannerImage} */}
-            <RightArrow />
+            <BannerImg img = {bannerImage[bannerImageIdx]} />
+            <RightArrow onClick={onIncreaseImage}/>
         </BannerWrapper>
     );
 }
